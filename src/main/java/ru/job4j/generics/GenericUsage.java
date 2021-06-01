b52fca7c445cc1191f2acf6f87d123c13cc76d00
Semyon Stanovov <sestanovov@gmail.com>
2021-06-01 11:58:33 +0300

    0. Что такое обобщенные типы (generics) [#4952] - Lecture

---
+++ src/main/java/ru/job4j/generics/GenericUsage.java
@@ -0,0 +1,36 @@
+package ru.job4j.generics;
+
+import java.util.*;
+
+public class GenericUsage {
+    public static void main(String[] args) {
+        GenericsClass<String, String> gen = new GenericsClass<>("First key", "First value");
+        System.out.println("Вывод в консоль: " + gen);
+
+        GenericsClass<Integer, String> second = new GenericsClass<>(12345, "Second value");
+        System.out.println("Вывод в консоль: " + second);
+    }
+
+    public void printRsl(Collection<?> col) {
+        for (Iterator<?> it = col.iterator(); it.hasNext();) {
+            Object next = it.next();
+            System.out.println(next);
+        }
+    }
+
+    public void printInfo(Collection<? extends Person> col) {
+        for (Iterator<? extends Person> it = col.iterator(); it.hasNext();) {
+            Person next = it.next();
+            System.out.println(next);
+        }
+    }
+
+    public void addAll(List<? super Integer> list) {
+        for (int i = 1; i <= 5; i++) {
+            list.add(i);
+        }
+        for (Object o : list) {
+            System.out.println("Текущий элемент: " + o);
+        }
+    }
+}
