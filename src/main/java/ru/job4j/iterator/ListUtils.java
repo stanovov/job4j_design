3cd7af4db445e324106774874f937439cc31760b
Semyon Stanovov <sestanovov@gmail.com>
2021-06-02 16:25:02 +0300

    7. ListIterator [#350217]

---
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -0,0 +1,68 @@
+package ru.job4j.iterator;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class ListUtils {
+
+    public static <T> void addBefore(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (i.nextIndex() == index) {
+                i.add(value);
+                break;
+            }
+            i.next();
+        }
+    }
+
+    public static <T> void addAfter(List<T> list, int index, T value) {
+        Objects.checkIndex(index, list.size());
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            i.next();
+            if (i.previousIndex() == index) {
+                i.add(value);
+                break;
+            }
+        }
+    }
+
+    public static <T> void removeIf(List<T> list, Predicate<T> filter) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            T value = i.next();
+            if (filter.test(value)) {
+                i.remove();
+            }
+        }
+    }
+
+    public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            if (filter.test(i.next())) {
+                i.set(value);
+            }
+        }
+    }
+
+    public static <T> void removeAll(List<T> list, List<T> elements) {
+        ListIterator<T> i = list.listIterator();
+        while (i.hasNext()) {
+            boolean remove = false;
+            T value = i.next();
+            for (T element : elements) {
+                if (Objects.equals(value, element)) {
+                    remove = true;
+                    break;
+                }
+            }
+            if (remove) {
+                i.remove();
+            }
+        }
+    }
+
+}
