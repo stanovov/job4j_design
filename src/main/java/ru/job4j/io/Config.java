05c873ddd921296914270e442766b3352955e15a
Semyon Stanovov <sestanovov@gmail.com>
2021-06-28 12:18:46 +0300

    1. Читаем файл конфигурации [#858 #171458] Fixed

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -3,10 +3,8 @@ package ru.job4j.io;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.util.stream.Collectors;
 
 public class Config {
 
@@ -19,20 +17,15 @@ public class Config {
 
     public void load() {
         try (BufferedReader in = new BufferedReader(new FileReader(path))) {
-            values.clear();
-            List<String> lines = in.lines()
+            in.lines()
                     .filter(s -> !s.isEmpty() && !s.startsWith("#"))
-                    .collect(Collectors.toList());
-            for (String line : lines) {
-                if (line.isEmpty() || line.startsWith("#")) {
-                    continue;
-                }
-                String[] entry = line.split("=");
-                if (entry.length != 2) {
-                    throw new IllegalArgumentException();
-                }
-                values.put(entry[0], entry[1]);
-            }
+                    .forEach(line -> {
+                        String[] entry = line.split("=");
+                        if (entry.length != 2) {
+                            throw new IllegalArgumentException();
+                        }
+                        values.put(entry[0], entry[1]);
+                    });
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException();
         } catch (Exception e) {
