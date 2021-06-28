842aa48373b9f1589b4f463987dc5af5e841dfc9
Semyon Stanovov <sestanovov@gmail.com>
2021-06-28 17:09:06 +0300

    4.2. Поиск дубликатов [#315066] Validations

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,15 +1,24 @@
 package ru.job4j.io.duplicates;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        if (args.length > 0) {
-            DuplicatesVisitor visitor = new DuplicatesVisitor();
-            Files.walkFileTree(Path.of(args[0]), visitor);
-            visitor.show();
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
         }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of(args[0]), visitor);
+        visitor.show();
     }
 }
