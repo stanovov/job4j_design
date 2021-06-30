e66e319a7e75e2e202b8ccbe1432f10fa04ff6a3
Semyon Stanovov <sestanovov@gmail.com>
2021-06-30 15:32:29 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -10,19 +10,31 @@ public class EchoServer {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(
-                             new InputStreamReader(socket.getInputStream()))) {
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    boolean messageFound = false;
                     boolean closed = false;
+                    String answer = "What";
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (!closed) {
-                            closed = str.contains("msg=Bye ");
+                        if (!messageFound) {
+                            int startIndex = str.indexOf("/?msg=");
+                            int lastIndex = str.lastIndexOf(" HTTP/1.1");
+                            if (startIndex != -1 && lastIndex != -1) {
+                                String message = str.substring(startIndex + 6, lastIndex);
+                                if (message.equals("Exit")) {
+                                    closed = true;
+                                } else if (message.equals("Hello")) {
+                                    answer = message;
+                                }
+                                messageFound = true;
+                            }
                         }
                         System.out.println(str);
                     }
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
                     if (closed) {
-                        out.write("SERVER CLOSED\r\n".getBytes());
                         server.close();
+                    } else {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write(answer.getBytes());
                     }
                 }
             }
