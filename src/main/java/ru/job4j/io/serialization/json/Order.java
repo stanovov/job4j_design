adb70a149b239e23d62c8b58be4ebafe53d69f83
Semyon Stanovov <sestanovov@gmail.com>
2021-07-01 18:35:33 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/io/serialization/json/Order.java
+++ src/main/java/ru/job4j/io/serialization/json/Order.java
@@ -2,6 +2,7 @@ package ru.job4j.io.serialization.json;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -34,6 +35,22 @@ public class Order {
             this.quantity = quantity;
         }
 
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public double getQuantity() {
+            return quantity;
+        }
+
         @Override
         public String toString() {
             return "Product{"
@@ -43,6 +60,40 @@ public class Order {
                     + ", quantity=" + quantity
                     + '}';
         }
+    }
+
+    public Order(String id, String name, boolean delivery,
+                 OrderStatus status, double price, Product[] products) {
+        this.id = id;
+        this.name = name;
+        this.delivery = delivery;
+        this.status = status;
+        this.price = price;
+        this.products = products;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isDelivery() {
+        return delivery;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Product[] getProducts() {
+        return products;
     }
 
     @Override
@@ -57,17 +108,7 @@ public class Order {
                 + '}';
     }
 
-    public Order(String id, String name, boolean delivery,
-                 OrderStatus status, double price, Product[] products) {
-        this.id = id;
-        this.name = name;
-        this.delivery = delivery;
-        this.status = status;
-        this.price = price;
-        this.products = products;
-    }
-
-    public static void main(String[] args) {
+    private static void exampleGSON() {
         Order order = new Order(
                 "1",
                 "New order",
@@ -87,26 +128,56 @@ public class Order {
         System.out.println(gson.toJson(order));
         String orderJson =
                 "{"
-                    + "\"id\":\"1\","
-                    + "\"name\":\"New order\","
-                    + "\"delivery\":true,"
-                    + "\"status\":\"SENT\","
-                    + "\"price\":260.0,"
-                    + "\"products\":["
+                        + "\"id\":\"1\","
+                        + "\"name\":\"New order\","
+                        + "\"delivery\":true,"
+                        + "\"status\":\"SENT\","
+                        + "\"price\":260.0,"
+                        + "\"products\":["
                         + "{"
-                            + "\"id\":\"product_1\","
-                            + "\"name\":\"Тетрадь в клетку 24 листа\","
-                            + "\"price\":111.0,"
-                            + "\"quantity\":2.0"
+                        + "\"id\":\"product_1\","
+                        + "\"name\":\"Тетрадь в клетку 24 листа\","
+                        + "\"price\":111.0,"
+                        + "\"quantity\":2.0"
                         + "},{"
-                            + "\"id\":\"product_2\","
-                            + "\"name\":\"Ручка черная гелиевая\","
-                            + "\"price\":38.0,"
-                            + "\"quantity\":1.0"
+                        + "\"id\":\"product_2\","
+                        + "\"name\":\"Ручка черная гелиевая\","
+                        + "\"price\":38.0,"
+                        + "\"quantity\":1.0"
                         + "}"
-                    + "]"
-                + "}";
+                        + "]"
+                        + "}";
         Order orderMod = gson.fromJson(orderJson, Order.class);
         System.out.println(orderMod);
+    }
+
+    private static void examplePOJO() {
+        Order order = new Order(
+                "1",
+                "New order",
+                true,
+                OrderStatus.NEW,
+                260.0,
+                new Product[]{
+                        new Product(
+                                "product_1",
+                                "Тетрадь в клетку 24 листа",
+                                111.0,
+                                2.0
+                        ),
+                        new Product(
+                                "product_2",
+                                "Ручка черная гелиевая",
+                                38.0,
+                                1.0
+                        )
+                }
+        );
+        JSONObject jsonObject = new JSONObject(order);
+        System.out.println(jsonObject.toString());
+    }
+
+    public static void main(String[] args) {
+        examplePOJO();
     }
 }
