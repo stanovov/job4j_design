08fa7928f21885461c84a81ecc57fe0a062981f4
Semyon Stanovov <sestanovov@gmail.com>
2021-07-01 17:27:39 +0300

    4. JAXB. Преобразование XML в POJO. [#315063] Lecture

---
+++ src/main/java/ru/job4j/io/serialization/xml/Person.java
@@ -0,0 +1,68 @@
+package ru.job4j.io.serialization.xml;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Arrays;
+
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Person {
+
+    @XmlAttribute
+    private boolean sex;
+
+    @XmlAttribute
+    private int age;
+
+    private Contact contact;
+
+    @XmlElementWrapper(name = "statuses")
+    @XmlElement(name = "status")
+    private String[] statuses;
+
+    public Person() { }
+
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
+        this.sex = sex;
+        this.age = age;
+        this.contact = contact;
+        this.statuses = statuses;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{"
+                + "sex=" + sex
+                + ", age=" + age
+                + ", contact=" + contact
+                + ", statuses=" + Arrays.toString(statuses)
+                + '}';
+    }
+
+    public static void main(String[] args) throws JAXBException, IOException {
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(person, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Person result = (Person) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+    }
+}
