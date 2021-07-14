f2345d34a650a51590ae67f59985dc81f1aaff31
Semyon Stanovov <sestanovov@gmail.com>
2021-07-14 15:44:24 +0300

    Фильтры [#1732 #177301] - Corrected

--- data/grocery_store_7.sql
+++ data/grocery_store_7.sql
@@ -1,6 +1,10 @@
 SELECT
 	t.name
-FROM product AS p
-	JOIN type AS t ON p.type_id = t.id
-GROUP BY t.name
-HAVING SUM(1) < 10
+FROM
+	product AS p
+		JOIN type AS t
+			ON p.type_id = t.id
+GROUP BY
+	t.name
+HAVING
+	COUNT(1) < 10
