d6c87b61dda4cdb8f7fe398a83f923d185bc41e8
Semyon Stanovov <sestanovov@gmail.com>
2021-08-11 22:28:04 +0300

    0. Виды ссылок [#6854 #188850]

---
+++ src/main/java/ru/job4j/gc/ref/SoftDemo.java
@@ -0,0 +1,46 @@
+package ru.job4j.gc.ref;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SoftDemo {
+
+    public static void main(String[] args) {
+        example1();
+        example2();
+    }
+
+    private static void example1() {
+        Object strong = new Object();
+        SoftReference<Object> soft = new SoftReference<>(strong);
+        strong = null;
+        System.out.println(soft.get());
+    }
+
+    private static void example2() {
+        List<SoftReference<Object>> objects = new ArrayList<>();
+        Object myobject = new Object();
+        objects.add(new SoftReference<>(myobject));
+        System.out.println(myobject);
+        for (int i = 0; i < 100_000_000; i++) {
+            objects.add(new SoftReference<>(new Object() {
+                String value = String.valueOf(System.currentTimeMillis());
+
+                @Override
+                protected void finalize() throws Throwable {
+                }
+            }));
+        }
+        System.gc();
+        int liveObject = 0;
+        for (SoftReference<Object> ref : objects) {
+            Object object = ref.get();
+            if (object != null) {
+                liveObject++;
+            }
+        }
+        System.out.println(liveObject);
+        System.out.println(myobject);
+    }
+}
