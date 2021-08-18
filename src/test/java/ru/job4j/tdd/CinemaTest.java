09e26df9f421524a09497d12af0026502d0c1445
Semyon Stanovov <sestanovov@gmail.com>
2021-08-18 16:07:26 +0300

    2. Что такое TDD? [#4918 #191028] fix

--- src/test/java/ru/job4j/tdd/CinemaTest.java
+++ src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.tdd;
 
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -21,12 +23,63 @@ public class CinemaTest {
         assertThat(ticket, is(new Ticket3D()));
     }
 
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketsForOneSeat() {
+        Account account1 = new AccountCinema();
+        Account account2 = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 11, 20, 12, 10);
+        cinema.buy(account1, 1, 1, date);
+        cinema.buy(account2, 1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketInThePast() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2018, 3, 20, 12, 10);
+        cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketWithIncorrectRow() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 3, 20, 12, 10);
+        cinema.buy(account, -99, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketWithIncorrectColumn() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 3, 20, 12, 10);
+        cinema.buy(account, 1, 100500, date);
+    }
+
     @Test
     public void find() {
         Cinema cinema = new Cinema3D();
         cinema.add(new Session3D());
         List<Session> sessions = cinema.find(session -> true);
         assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    @Ignore
+    @Test
+    public void whenCouldNotFind() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertThat(sessions, is(List.of()));
     }
 
     @Test
